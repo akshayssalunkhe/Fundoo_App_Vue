1c9a64bcc936cccb8c739f1cf42137a5d94195a6
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-28 13:10:24 +0530

    [Akshay]:Add:Note Card Component

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,96 @@
+<template>
+<div >
+    <md-card class="NoteCard">
+     
+      <md-card-header class="Title-input">
+        <div class="md-title">
+            <md-field>
+                <md-input v-model="Title" placeholder="Title...">                
+                </md-input>
+                
+                <md-button class="md-icon-button">
+                  <md-icon>person_add</md-icon>
+                </md-button>
+            </md-field>
+          </div>
+      </md-card-header>
+
+      <md-card-content class="content-input">
+       <md-field>
+            <md-textarea v-model="content" placeholder="Take a Note.."></md-textarea>
+          </md-field>
+      </md-card-content>
+
+      <md-card-actions>
+          <md-button class="md-icon-button">
+                  <md-icon>notifications</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>person_add</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>palette</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>panorama</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>archive</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>more_vert</md-icon>
+                </md-button>
+        <md-button>Close</md-button>
+      </md-card-actions>
+    </md-card>
+</div>
+</template>
+
+<script>
+export default {
+    name:"NoteCard",
+ data() {
+    return {
+      Title:"",
+      content: "",
+     };
+  },
+  }
+</script>
+
+<style scoped>
+.NoteCard{
+    background-color:white;
+    flex: 1 1 500px;
+    box-sizing: border-box;
+    border-radius: 5%;
+    max-width: 500px;
+    min-width: 500px;
+    margin-left: 20%;
+    margin-top: 5%;
+}
+.md-field.md-has-textarea:not(.md-autogrow) .md-textarea {
+    min-height: 100px;
+    padding: 0 9px;
+    resize: vertical;
+}
+.md-field {
+    margin: 0px;
+}
+.Title-input,.content-input{
+    border: none;
+    outline: none;
+    width:86%;
+}
+.md-card-content {
+    padding: 0px;
+    font-size: 14px;
+    line-height: 22px;
+}
+.md-card-header {
+    padding: 10px;
+}
+.md-card-actions.md-alignment-right {
+    justify-content: space-around;
+}
+</style>
