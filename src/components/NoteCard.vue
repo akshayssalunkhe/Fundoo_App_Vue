f78f2d2126e92af94ccd021fcb1d1f82d9401603
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-29 17:29:51 +0530

    [Akshay]:Refactor:Code To Retrive Data From Back-End

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,66 +1,50 @@
 <template>
-<div >
- <div class="note-cards" v-for=" note in noteList " v-bind:key= "note">
 
-    <md-card class="NoteCard">
-            <md-card-header class="Title-input">
-              <div class="md-title">
-                <md-field>
-                  <md-input placeholder="Title...">{{note.title}}</md-input>
-                
-                  <md-button class="md-icon-button">
-                    <md-icon>add</md-icon>
-                  </md-button>
-                </md-field>
-              </div>
-            </md-card-header>
+<div class="display-all-notes">
+    <div class="note-cards" v-for="(note,index) in noteList" v-bind:key="index">
+      <md-card>
+        <label class="title">{{ note.title }}</label
+        ><br />
+        <label class="description">{{ note.description }}</label
+        ><br />
+        <div class="notebox-icons">
+                <ArchiveIcon></ArchiveIcon>
+                <ColorPallete></ColorPallete>
+                <DeleteIcon></DeleteIcon>
+                <MoreVertIcon></MoreVertIcon>
 
-            <md-card-content class="content-input">
-              <md-field>
-                <md-textarea placeholder="Take a Note..">{{note.description}}</md-textarea>
-              </md-field>
-            </md-card-content>
-
-              <md-card-actions>
-                <md-button class="md-icon-button">
-                  <md-icon>notifications</md-icon>
-                </md-button>
-                <md-button class="md-icon-button">
-                  <md-icon>person_add</md-icon>
-                </md-button>
-                <md-button class="md-icon-button">
-                  <md-icon>palette</md-icon>
-                </md-button>
-                <md-button class="md-icon-button">
-                  <md-icon>panorama</md-icon>
-                </md-button>
-                <md-button class="md-icon-button">
-                  <md-icon>archive</md-icon>
-                </md-button>
-                <md-button class="md-icon-button">
-                  <md-icon>more_vert</md-icon>
-                </md-button>
-                <md-button>Close</md-button>
-          </md-card-actions>
-        </md-card>
-      </div>
-</div>
+        </div>
+      </md-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import MoreVertIcon from './Icons/MoreVertIcon'
+import DeleteIcon from './Icons/DeleteIcon'
+import ColorPallete from './Icons/ColorPallete'
+import ArchiveIcon from './Icons/ArchiveIcon'
 import UserService from '../services/UserService'
 export default {
   name: "NoteCard",
   data() {
     return {
+
       noteList: [],
     };
+    
   },
+  components:{
+      ColorPallete,
+      ArchiveIcon,
+      DeleteIcon,
+      MoreVertIcon,
+    },
   methods: {
     fetchNotes: function () {
       UserService.fetchNotesList().then((response) => {
-        this.noteList = response.data;
-        console.log("fetch notes function ")
+        this.noteList = response.data.data.data;
+        console.log(this.noteList)
       });
     },
   },
@@ -72,38 +56,42 @@ export default {
 </script>
 
 <style scoped>
-.NoteCard{
-    background-color:white;
-    flex: 1 1 500px;
-    box-sizing: border-box;
-    border-radius: 3%;
-    max-width: 500px;
-    min-width: 500px;
-    margin-left: 20%;
-    margin-top: 5%;
+.display-all-notes {
+  justify-content: center;
+  display: flex;
+  margin-top: 2%;
+  margin-left: 10%;
+  flex-direction: row;
+  max-width: 100%;
+  min-width: 70%;
+  flex-wrap: wrap;
 }
-.md-field.md-has-textarea:not(.md-autogrow) .md-textarea {
-    min-height: 100px;
-    padding: 0 9px;
-    resize: vertical;
+
+.md-card {
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5%;
+  height: min-content;
+  width: 190px;
+  text-align: start;
+  padding: 18px;
 }
-.md-field {
-    margin: 0px;
+.title {
+  font-weight: bold;
+  font-size: 18px;
+  max-width: 90%;
+  opacity: 0.6;
+  cursor: text;
+  margin: 0px 0px 10px 0px;
 }
-.Title-input,.content-input{
-    border: none;
-    outline: none;
-    width:86%;
+.description {
+  font-size: 16px;
+  font-weight: 500;
 }
-.md-card-content {
-    padding: 0px;
-    font-size: 14px;
-    line-height: 22px;
-}
-.md-card-header {
-    padding: 10px;
-}
-.md-card-actions.md-alignment-right {
-    justify-content: space-around;
+.notebox-icons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 </style>
