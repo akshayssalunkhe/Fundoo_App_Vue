9ee9e5cb2f27169e44d3e6b259f59618a55a8fcd
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-30 23:27:14 +0530

    [Akshay]:Add:Component To Display Note List And Create Note

---
+++ src/components/Notes.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <CreateNote></CreateNote>
+    <DisplayNotes v-bind:noteList="noteList"></DisplayNotes>
+</div>
+</template>
+<script>
+import CreateNote from './CreateNote'
+import DisplayNotes from './DisplayNotes'
+import UserService from '../services/UserService'
+export default {
+    name:"Notes",
+data() {
+    return {
+      noteList: [],
+    };
+    
+  },
+  components:{
+      DisplayNotes,
+      CreateNote,
+    },
+  methods: {
+    fetchNotes: function () {
+      UserService.fetchNotesList().then((response) => {
+        this.noteList = response.data.data.data;
+        console.log(this.noteList)
+      });
+    },
+  },
+  mounted() {
+    this.fetchNotes();
+    console.log(this.noteList)
+  },
+};
+</script>
+<style scoped>
+
+</style>
