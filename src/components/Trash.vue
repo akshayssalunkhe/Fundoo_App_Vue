fdf356c0ad3ec352f71a1224eb9baa1a8ea51e68
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-30 23:23:44 +0530

    [Akshay]:Add:Component To Display Trash Notes

---
+++ src/components/Trash.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="archive-main flex-properties">
+    <DisplayNotes v-bind:noteList="trashList"></DisplayNotes>
+  </div>
+</template>
+<script>
+import DisplayNotes from '../components/DisplayNotes'
+import UserService from "../services/UserService";
+export default {
+  name: "Trash",
+  data() {
+    return {
+      trashList: [],
+    };
+  },
+  components:{
+      DisplayNotes,
+  },
+  
+  methods: {
+    fetchTrashList: function () {
+      UserService.fetchTrashNotesList().then((response) => {
+        this.trashList = response.data.data.data;
+        console.log(this.trashList)
+        alert("Trash fetchnotesList")
+      });
+    },
+  },
+  created() {
+    this.fetchTrashList();
+  },
+};
+</script>
+<style scoped>
+.flex-properties {
+  display: flex;
+  flex-direction: row;
+}
+
+</style>
